062b1a4644673bafa119ea7a40cdf1f6e3e26de3
zsiki <siki@agt.bme.hu>
2015-11-21 04:16:44 +0100

    initial commit

---
+++ i18n/hu.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="attribute_split.py" line="192"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split.py" line="192"/>
+        <source> unique values! Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AttributeSplit</name>
+    <message>
+        <location filename="attribute_split.py" line="166"/>
+        <source>&amp;Attribute Split</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split.py" line="156"/>
+        <source>Attribute split</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AttributeSplitDialogBase</name>
+    <message>
+        <location filename="attribute_split_dialog_base.ui" line="14"/>
+        <source>Attribute Split</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split_dialog_base.ui" line="42"/>
+        <source>Layer name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split_dialog_base.ui" line="55"/>
+        <source>Column name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split_dialog_base.ui" line="98"/>
+        <source>Prefix for file names:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split_dialog_base.ui" line="111"/>
+        <source>Target directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attribute_split_dialog_base.ui" line="140"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
